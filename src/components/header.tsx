c38501d2dcb50470ae2e5dde0e2be03d76b2ecce
AstroMev <35166412+AstroMev@users.noreply.github.com>
2026-05-25 14:50:47 +0900

    fix: ヘッダーのレスポンシブ対応

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,8 +6,8 @@ import NavLink from "./NavLink";
 export default function Header() {
   return (
     <header className="bg-gradient-to-br from-pearl-bush-50 to-pearl-bush-100 text-black py-3 px-2 sm:p-4 w-full shadow-lg sticky top-0 z-50 border-b border-mischka-200">
-      <div className="max-w-6xl mx-auto px-2 sm:px-4">
-        <div className="flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-4">
+      <div className="max-w-6xl mx-auto px-2">
+        <div className="flex flex-col sm:flex-row justify-between items-center gap-2">
           <Link href="/#top" className="text-lg sm:text-xl md:text-2xl font-bold">
             <div className="flex items-end gap-2">
               <Image src="/logo.webp" width="28" height="28" alt="" className="rounded" />
@@ -15,7 +15,7 @@ export default function Header() {
             </div>
           </Link>
           <nav>
-            <ul className="flex gap-1 sm:gap-2 md:gap-4">
+            <ul className="flex flex-wrap justify-center gap-1 sm:gap-2 md:gap-4">
               <NavLink href="/#services">サービス</NavLink>
               <NavLink href="/#strengths">強み</NavLink>
               <NavLink href="/#area">対応地域</NavLink>
